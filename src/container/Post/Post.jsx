ad249580d5419b7aab731797c3228244baade570
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2021-12-10 14:22:00 +0800

    Update Post Data to API

--- src/container/Post/Post.jsx
+++ src/container/Post/Post.jsx
@@ -4,22 +4,48 @@ import axios from "axios";
 
 class Post extends Component{
     state = {
-        post: []
+        post: [],
+        formPost: {
+            id:1,
+            title:'',
+            body:'',
+            userId:1
+        } 
     }
 
     componentDidMount(){
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        // .then(response => response.json())
-        // .then(json => {
-        //     this.setState({
-        //         post: json
-        //     })
-        // })
         this.getPostAPI();
     }
 
+    handleFormChange = (event) => {
+        let time = new Date().getTime();
+        let formPostNew = {...this.state.formPost};
+        formPostNew[event.target.name] = event.target.value;
+        formPostNew['id'] = time;
+        this.setState({
+            formPost: formPostNew
+        });
+    }
+
+    handleSubmit = () => {
+        let time = new Date().getTime();
+        let formPostNew = {...this.state.formPost};
+        formPostNew['id'] = time;
+        this.setState({
+            formPost: formPostNew
+        });
+
+        axios.post('http://localhost:3004/posts', this.state.formPost)
+        .then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            document.getElementById("title").value='';
+            document.getElementById("body").value='';
+        });
+    }
+
     getPostAPI = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then((res) => {
             console.log(res);
             this.setState({
@@ -39,7 +65,18 @@ class Post extends Component{
     render(){
         return(
             <Fragment>
-                <p>Post With API</p>
+                <p>Consume API</p>
+                <form >
+                    <div className="form-group">
+                        <label htmlFor="">Title</label>
+                        <input className="form-control" type="text" name="title" id="title" onChange={this.handleFormChange}/>
+                    </div>
+                    <div className="form-group">
+                        <label  htmlFor="">Body</label>
+                        <textarea className="form-control" name="body" id="body" cols="30" rows="10" onChange={this.handleFormChange}></textarea>  
+                    </div>
+                    <button className="btn btn-primary" type="button" onClick={this.handleSubmit}>Simpan</button>
+                </form>
                 {
                     this.state.post.map(post => {
                         return <ItemPost key={post.id} data={post} remove={this.handleRemove}/>
