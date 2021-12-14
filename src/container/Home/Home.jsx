afb22af31f43e7dbc34fc12f2850b7a3bcdef386
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2021-12-14 10:15:58 +0800

    Update Home.jsx

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -9,24 +9,6 @@ import Navbar from "../../component/Navbar/Navbar";
 class Home extends Component {
     render() {
         return (
-            // <div >
-            //     {/* <p>YT Component</p>
-            //     <hr />
-            //     <div className="d-flex">
-            //         <YoutubeComponent title="Vid 1" desc="ini vid 1"/>
-            //         <YoutubeComponent title="Vid 2" desc="ini vid 2"/>
-            //         <YoutubeComponent />
-            //         <YoutubeComponent />
-            //         <YoutubeComponent/>
-            //     </div>
-            //     <div>
-            //         <Total/>
-            //     </div> */}
-            //     {/* <div>
-            //         <LifeCycleComp />
-            //     </div> */}
-            //     <Post />
-            // </div>
             <Fragment>
                 <Navbar />
             </Fragment>
